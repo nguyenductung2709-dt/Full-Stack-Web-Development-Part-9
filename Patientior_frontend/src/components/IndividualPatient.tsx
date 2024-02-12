2bc0cb653e19310cb57a64f64dc5ec51055aca4f
Duc Tung Nguyen <tungdtnguyen2709@gmail.com>
2024-02-12 11:40:44 +0200

    9.23

--- Patientior_frontend/src/components/IndividualPatient.tsx
+++ Patientior_frontend/src/components/IndividualPatient.tsx
@@ -1,4 +1,4 @@
-import { Patient } from '../types';
+import { Patient, Entry } from '../types';
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from 'react';
 import patientService from '../services/patients';
@@ -28,15 +28,76 @@ const IndividualPatient = () => {
     };
   }, [id]);
 
+  const renderEntry = (entry: Entry) => {
+    switch (entry.type) {
+      case "Hospital":
+        return (
+          <div key={entry.id}>
+            <p>
+              <span>{entry.date}</span> <span> <i>{entry.description}</i> </span>{" "}
+            </p>
+            {entry.diagnosisCodes && (
+              <ul>
+                {entry.diagnosisCodes.map((diagnosis, index) => (
+                  <li key={index}>{diagnosis}</li>
+                ))}
+              </ul>
+            )}
+            {entry.discharge && (
+              <p>Discharge Date: {entry.discharge.date}</p>
+            )}
+          </div>
+        );
+      case "OccupationalHealthcare":
+        return (
+          <div key={entry.id}>
+            <p>
+              <span>{entry.date}</span> <span> <i>{entry.description}</i> </span>{" "}
+            </p>
+            {entry.diagnosisCodes && (
+              <ul>
+                {entry.diagnosisCodes.map((diagnosis, index) => (
+                  <li key={index}>{diagnosis}</li>
+                ))}
+              </ul>
+            )}
+            {entry.sickLeave && (
+              <p>Sick Leave: {entry.sickLeave.startDate} - {entry.sickLeave.endDate}</p>
+            )}
+          </div>
+        );
+      case "HealthCheck":
+        return (
+          <div key={entry.id}>
+            <p>
+              <span>{entry.date}</span> <span> <i>{entry.description}</i> </span>{" "}
+            </p>
+            {entry.healthCheckRating && (
+              <p>Health Check Rating: {entry.healthCheckRating}</p>
+            )}
+          </div>
+        );
+      default:
+        return null;
+    }
+  };
+
   if (!patient) {
     return <div>Loading...</div>; 
   }
 
   return (
     <>
-      <h2>{patient.name} <span> {patient.gender === "male" ? <MaleIcon /> : <FemaleIcon />} </span></h2>
-      <p>ssh: {patient.ssn}</p>
+      <h2>
+        {patient.name}{" "}
+        <span>{patient.gender === "male" ? <MaleIcon /> : <FemaleIcon />}</span>
+      </h2>
+      <p>ssn: {patient.ssn}</p>
       <p>occupation: {patient.occupation}</p>
+      <p>
+        <strong> entries </strong>
+      </p>
+      {patient.entries.map(renderEntry)}
     </>
   );
 };
--- Patientior_frontend/src/types.ts
+++ Patientior_frontend/src/types.ts
@@ -13,10 +13,57 @@ export enum Gender {
 export interface Patient {
   id: string;
   name: string;
+  ssn: string;
   occupation: string;
   gender: Gender;
-  ssn?: string;
-  dateOfBirth?: string;
+  dateOfBirth: string;
+  entries: Entry[]
 }
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis['code']>;
+}
+
+interface Discharge {
+  date: string;
+  criteria: string;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: Discharge;
+}
+
+interface SickLeave {
+  startDate: string;
+  endDate: string;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: SickLeave;
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
 
 export type PatientFormValues = Omit<Patient, "id" | "entries">;
